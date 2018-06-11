7e38f5dac868c94a11c65a2d00f3e7a30b380177
Akbar Abustang <akbar_abustang@ymail.com>
2018-06-11 17:56:31 +0700

    laravel vue

---
+++ resources/assets/js/app.js
@@ -0,0 +1,11 @@
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('navbar', require('./components/Navbar.vue'));
+Vue.component('articles', require('./components/Articles.vue'));
+
+const app = new Vue({
+    el: '#app'
+});
---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="navbar navbar-expand-sm dark bg-info mb-2">
+        <div class="container">
+            <a href="#" class="navbar-brand" style="color:white;" >Laravel Vue</a>
+        </div>
+    </nav>
+</template>
